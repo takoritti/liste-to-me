7d06a31d538f69b8df083efc6e6d30689fbb56d6
takoritti <s94.owari.takashi@gmail.com>
2021-03-17 16:53:50 +0900

    新規登録機能実装

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,4 +1,24 @@
 class MusicsController < ApplicationController
+
   def index
+    # @musics = Music.includes(:user)
+  end
+
+  def new
+    @music = Music.new
+  end
+
+  def create
+    @music = Music.new(music_params)
+    if @music.save
+      redirect_to musics_path(@musics)
+    else
+      render :new
+    end
+  end
+
+  private
+  def music_params
+    params.require(:music).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
