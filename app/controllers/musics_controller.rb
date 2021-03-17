5b073d9218e0a03233c1af785ca821a6e1af50fc
takoritti <s94.owari.takashi@gmail.com>
2021-03-17 17:05:25 +0900

    ログアウトボタン実装

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -17,6 +17,37 @@ class MusicsController < ApplicationController
     end
   end
 
+  def show
+    @music = Music.find(params[:id])
+    @comment = Comment.new
+    @comments = @music.comments.includes(:user)
+
+  end
+
+  def edit
+    @music = Music.find(params[:id])
+    unless @music.user.id == current_user.id
+    redirect_to action: :index
+    end
+  end
+
+  def update
+    @music = Music.find(params[:id])
+    music = Music.find(params[:id])
+    if 
+      music.update(music_params)
+      redirect_to music_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    music = Music.find(params[:id])
+    music.destroy
+    redirect_to root_path 
+  end
+
   private
   def music_params
     params.require(:music).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
